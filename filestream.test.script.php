084681ae81fc6e7ffc8fd8d2228e8b008cdb11dc
Hect0r Xorius <sysop@pkx.pw>
2017-05-26 20:43:21 +0100

    Create filestream.test.script.php

---
+++ filestream.test.script.php
@@ -0,0 +1,42 @@
+#!/bin/bash
+<?php
+
+include('fileStream.php');
+
+// Create new file to write too.
+$handle = new fileStream('test.bin', 'c');
+
+// Tell if we failed to open it due to permissions.
+if(!$handle->isOpen()) { echo "Unable to open file for creation.\n"; exit(); }
+
+// Goto position zero.
+$handle->setPosition(0);
+
+// Test write some stuff.
+$handle->writeAsciiString('HELLO');
+$handle->writeUInt16(20);
+$handle->writeUInt32(20);
+$handle->writeUInt64(9223372036854775807);
+// Closed the newly created filestream.
+$handle->close();
+
+// Reopen said file, with read permissions.
+$handle = new fileStream('test.bin', 'r');
+
+// Goto position zero.
+$handle->setPosition(0);
+
+// Dump to the console each value we wrote above...
+var_dump($handle->readAsciiString(5));
+var_dump($handle->readUInt16());
+var_dump($handle->readUInt32());
+var_dump($handle->readUInt64());
+
+// Close to save memory leaks.
+$handle->close();
+// Optionable delete said file after.
+//unlink('test.bin');
+// Cleanup pointers.
+unset($handle);
+
+?>
