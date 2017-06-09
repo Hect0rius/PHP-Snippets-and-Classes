9c6f2b544cc254760a3ccee4f25ed6b5e20ca4cc
Hect0r Xorius <sysop@pkx.pw>
2017-06-09 20:40:00 +0100

    New Upload.

---
+++ AES.CBC.256.php
@@ -0,0 +1,27 @@
+<?php
+
+// AES CBC 256 Encrypt Plain text with key string.
+// Returns, Array with ciphered text and iv.
+function aesCbc256encrypt($plainText, $keyStr) {
+    if(strlen($keyStr) !== 64) {
+        return -1;
+    }
+    
+    $key = pack('H*', $keyStr);
+    $iv = mcrypt_create_iv(32, MCRYPT_RAND);
+    
+    return array(
+        'ciphered' => mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $plainText, MCRYPT_MODE_CBC, $iv),
+        'iv' => $iv
+    );
+}
+
+// AES CBC 256, Decrypt Encrypted Text with key string and iv.
+function aesCbc256Decrypt($ciphered, $keyStr, $iv) {
+    if(strlen($keyStr) !== 64) { return -1; }
+    
+    $key = pack('H*', $keyStr);
+    
+    return mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $ciphered, MCRYPT_MODE_CBC, $iv);
+}
+?>
