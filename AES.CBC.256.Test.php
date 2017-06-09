1a5e9d8c273947d67ab4652753ba1989729b3325
Hect0r Xorius <sysop@pkx.pw>
2017-06-09 21:13:42 +0100

    Initial Commit

---
+++ AES.CBC.256.Test.php
@@ -0,0 +1,17 @@
+<?php
+
+include('AES.CBC.256.php');
+
+$keyStr = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+
+$cipheredData = aesCbc256encrypt('Hello World', $keyStr);
+
+echo 'Ciphered : ' . $cipheredData['ciphered']) . "\n";
+echo 'IV : ' . $cipheredData['iv'] . "\n";
+
+$unciphered = aesCbc256decrypt($cipheredData['ciphered'], $keyStr, $cipheredData['iv']);
+
+echo "Unciphered : " . $unciphered . "\n";
+echo "Done :)\n";
+
+?>
